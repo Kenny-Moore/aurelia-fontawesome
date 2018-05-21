d1e933e9f77f8fed47c0c7bca69395293729c3d2
gooeyideas <kenneth.moore@ungerboeck.com>
2018-05-21 12:13:57 -0500

    Updating the normalize function
    The icon binding will now parse static binding input as json and it will split comma delemented strings to catch icon names and prefixes.

--- src/utils/normalize-icon-spec.util.js
+++ src/utils/normalize-icon-spec.util.js
@@ -11,15 +11,25 @@ export const faNormalizeIconSpec = (iconSpec) => {
     return null;
   }
 
+  try {
+    iconSpec = JSON.parse(iconSpec.replace(/'/g, '"'));
+  } catch (e) {
+    // Oh well, but whatever...
+  }
+
   if (isIconLookup(iconSpec)) {
     return iconSpec;
   }
 
-  if (Array.isArray(iconSpec) && (iconSpec).length === 2) {
-    return { prefix: iconSpec[0], iconName: iconSpec[1] };
+  if (typeof iconSpec === 'string') {
+    if (iconSpec.indexOf(',') > -1) {
+      iconSpec = iconSpec.split(',');
+    } else {   
+      return { prefix: defaultPrefix, iconName: iconSpec };
+    }
   }
 
-  if (typeof iconSpec === 'string') {
-    return { prefix: defaultPrefix, iconName: iconSpec };
+  if (Array.isArray(iconSpec) && (iconSpec).length === 2) {
+    return { prefix: iconSpec[0], iconName: iconSpec[1] };
   }
 };
